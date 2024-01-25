00522c8ef0184e0fa764ca832ef11dbe9f22f1ad
AntonioSun <asun@ats.l5.ca>
2024-01-25 17:35:04 -0500

    - [+] add individual load test ledger_data

---
+++ Ledger_data/ledger_data_ins.groovy
@@ -0,0 +1,27 @@
+
+fragment {
+    simple {
+
+      // Defines a 'Random Variable' config element
+      random name: 'Random ledger index number', variable: 'ledgerIndex', minimum: 71899701, maximum: 85478162, perUser: true
+
+      debug '--== Tx: ledger_data ==--', displayJMeterVariables: true, displayJMeterProperties: true, enabled: false
+      transaction('Tx01 ledger_data', generate: true) {
+        
+        http (method: 'POST', path: '/', name: 'Tx01r ledger_data') {
+          body '''{"method":"ledger_data","params": [{"ledger_index":"${ledgerIndex}","binary":false}]}'''
+          //extract_jmes expression: 'book.id', variable: 'p_bookId'
+        }
+    
+      }
+
+      flow (name: 'Think Time Flow Control', enabled: false) {
+        uniform_timer (name: 'Think Time', delay: '${c_tt_delay}', range: '${c_tt_range}')
+      }
+
+      flow (name: 'Pace Time Flow Control') {
+        uniform_timer (name: 'Pace Time', delay: '${c_tt_delay}', range: '${c_tt_range}')
+      }
+
+    }
+}
---
+++ Ledger_data/load_settings.groovy
@@ -0,0 +1,5 @@
+class load_settings {
+    def static v = [
+        ledger_data: [ users: 1, ramp: 10, delay: 0, loops: 1, duration: 120, scheduler: true, enabled: true ],
+    ]
+}

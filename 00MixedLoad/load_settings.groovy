cc3bc5f2f17bd986efed2421e83c93acc1083f22
AntonioSun <asun@ats.l5.ca>
2024-01-27 00:28:43 -0500

    - [+] use env var to initial mixedLoad load setting

--- 00MixedLoad/load_settings.groovy
+++ 00MixedLoad/load_settings.groovy
@@ -1,8 +1,13 @@
 class load_settings {
+
+    //def static tusers  =  System.env['TEST_USERS'] ?: 1
+    def static tramp  =  System.env['TEST_RAMP'] ?: 10
+    def static tduration = System.env['TEST_DURATION'] ?: 120
+
     def static v = [
-        server_info: [ users: 1, ramp: 10, delay: 0, loops: 1, duration: 120, scheduler: true, enabled: true ],
-        account_info: [ users: 1, ramp: 10, delay: 0, loops: 1, duration: 120, scheduler: true, enabled: true ],
-        nft_info: [ users: 1, ramp: 10, delay: 0, loops: 1, duration: 120, scheduler: true, enabled: true ],
-        ledger_data: [ users: 1, ramp: 10, delay: 0, loops: 1, duration: 120, scheduler: true, enabled: true ],
+        server_info: [ users: 1, ramp: tramp, delay: 0, loops: 1, duration: tduration, scheduler: true, enabled: true ],
+        account_info: [ users: 1, ramp: tramp, delay: 0, loops: 1, duration: tduration, scheduler: true, enabled: true ],
+        nft_info: [ users: 1, ramp: tramp, delay: 0, loops: 1, duration: tduration, scheduler: true, enabled: true ],
+        ledger_data: [ users: 1, ramp: tramp, delay: 0, loops: 1, duration: tduration, scheduler: true, enabled: true ],
     ]
 }

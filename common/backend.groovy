5efc0a710d565c69a44965653485b20e73e8bfe7
AntonioSun <asun@ats.l5.ca>
2024-02-22 12:28:01 -0500

    - [#] common groovy scripts use copy instead of symlink

--- common/backend.groovy
+++ common/backend.groovy
@@ -1,1 +1,18 @@
-../demo_reuse/fragments/backend.groovy
+fragment {
+    backend(name: 'InfluxDb Backend', classname: 'rocks.nt.apm.jmeter.JMeterInfluxDBBackendListenerClient', enabled: false) {
+      arguments {
+        argument(name: 'testName', value: '${c_cfg_TestName}')
+        argument(name: 'nodeName', value: '${c_cfg_TestName} - users: ${c_lt_users}, duration ${c_lt_duration}, rampup: ${c_lt_ramp}')
+        argument(name: 'runId', value: '''${c_cfg_TestName}_${__time(yyMMdd-HHmm)}''')
+        argument(name: 'influxDBHost', value: '${c_cfg_Influxdb}')
+        argument(name: 'influxDBPort', value: '8086')
+        argument(name: 'influxDBUser', value: 'admin')
+        argument(name: 'influxDBPassword', value: 'Admin4perf@Ripp13%123')
+        argument(name: 'influxDBDatabase', value: 'jmeter')
+        argument(name: 'retentionPolicy', value: 'autogen')
+        argument(name: 'samplersList', value: '^Tx\\d+.*')
+        argument(name: 'useRegexForSamplerList', value: 'true')
+        argument(name: 'recordSubSamples', value: 'true')
+      }
+    }
+}
--- common/stationary-end.groovy
+++ common/stationary-end.groovy
@@ -1,1 +1,6 @@
-../demo_reuse/fragments/stationary-end.groovy
+fragment {
+    debug '---- Thread Groups ends ----', enabled: false
+    insert 'fragments/backend.groovy'
+    summary(file: '''${c_cfg_TestName}_${__time(yyMMdd-HHmm)}.jtl''', enabled: true)
+    view () // View Result Tree
+}

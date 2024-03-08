624b1870d2ffd50986e6691c4fbc851d1a53c103
AntonioSun <asun@ats.l5.ca>
2024-03-08 16:13:31 -0500

    - [#] simplify groovy script using common fragments

--- 00mixedload/xrpl-mixedLoad.groovy
+++ 00mixedload/xrpl-mixedLoad.groovy
@@ -28,39 +28,16 @@ start {
       variable(name: 'c_pt_range', value: '${__P(c_pt_range, 12000)}', description: 'Pace Time: Maximum random number of ms to delay')
       variable(name: 'c_pt_delay', value: '${__P(c_pt_delay, 3000)}', description: 'Pace Time: Ms to delay in addition to random time')
       variable(name: 'c_cfg_TestName', value: 'server_info', description: 'Test name to identify different tests')
-      variable(name: 'c_cfg_TimeTag', value: '_${START.YMD}-${START.HMS}', description: 'Time based tag to identify different tests')
       variable(name: 'c_cfg_Influxdb', value: '${__env(c_cfg_Influxdb, , localhost)}', description: 'Influxdb server host name')
       variable(name: 'p_session_email', value: 'john')
       variable(name: 'p_session_password', value: 'john')
       }
-    variables {
-      variable(name: 'c_cfg_TestID', value: '${c_cfg_TestName}${c_cfg_TimeTag}', description: 'Test ID, for identification and reporting purpose')
-    }
 
-    csv name: 'CSV accounts', file: 'accounts.csv', variables: ['s_account']
-    csv name: 'CSV nft_ids', file: 'nft_ids.csv', variables: ['s_nft_id']
+    csv name: 'CSV accounts', file: '../common/accounts.csv', variables: ['s_account']
+    csv name: 'CSV nft_ids', file: '../common/nft_ids.csv', variables: ['s_nft_id']
 
-    debug '---- default request settings ----', enabled: false
-    defaults(protocol: '${c_app_protocol}', domain: '${c_app_host_name}', port:  '${c_app_host_port}')
-    headers {
-      header(name: 'Host', value: '${c_app_host_name}')
-      header(name: 'Origin', value: '${c_app_host_name}')
-      header(name: 'Referer', value: '${c_app_host_name}')
-      header(name: 'Connection', value: 'keep-alive')
-      header(name: 'Cache-Control', value: 'max-age=0')
-      header(name: 'Upgrade-Insecure-Requests', value: '1')
-      header(name: 'User-Agent', value: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36')
-    }
-    cookies()
-    cache()
-
-    debug '---- default error checkings ----', enabled: false
-    check_response {
-      status() eq 200 or 302
-    }
-    check_response applyTo: 'parent', {
-      text() excludes '${c_app_error_kw}'
-    }
+    // common file-beg configuration
+    insert 'common/stationary-beg.groovy'
 
     debug '---- Thread Groups starts ----', enabled: false
     group(name: 'Thread Group server_info', delay: load_settings.v.server_info.delay, delayedStart: true,
@@ -134,22 +111,7 @@ start {
       insert 'Ledger_data/ledger_data_ins.groovy'
     }
 
-    debug '---- Thread Groups ends ----', enabled: false
-
-    backend(name: 'InfluxDb Backend', enabled: false) {
-      arguments {
-        argument(name: 'influxdbMetricsSender', value: 'org.apache.jmeter.visualizers.backend.influxdb.HttpMetricsSender')
-        argument(name: 'influxdbUrl', value: 'http://${c_cfg_Influxdb}:8086/write?db=jmeter_results')
-        argument(name: 'application', value: '${c_cfg_TestID}')
-        argument(name: 'measurement', value: 'jmeter')
-        argument(name: 'summaryOnly', value: 'false')
-        argument(name: 'samplersRegex', value: '^Tx\\d+.*')
-        argument(name: 'percentiles', value: '50;90;95;99')
-        argument(name: 'testTitle', value: '${c_cfg_TestID} - users: ${c_lt_users}, duration ${c_lt_duration}, rampup: ${c_lt_ramp}')
-        argument(name: "eventTags", value: '')
-      }
-    }
-    summary(file: '${c_cfg_TestID}.jtl', enabled: notProd)
-    view (enabled: notProd) // View Result Tree
+  // common file-end configuration
+  insert 'common/stationary-end.groovy'
   }
 }
